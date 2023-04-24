4a1ac52791e68e583029c580be583364d9a8098c
sean <midruss@yahoo.com>
2023-04-24 17:17:18 -0400

    start animals.ts file

--- Practice-problems/src/animals.js
+++ Practice-problems/src/animals.js
@@ -1,46 +1,34 @@
-import { dog, cat, bird, pigeon, anyAnimal } from "./animals_data";
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // Part 1: Remove the errors in the function below by filling the conditions of the if statements on lines 6, 13, and 20
-function classifyAnimal(animal: anyAnimal) {
-
+function classifyAnimal(animal) {
     if ('barks' in animal) {
-
-        animal.barks
-        console.log('This is a dog')
-        return
+        animal.barks;
+        console.log('This is a dog');
+        return;
     }
-
     if ('coos' in animal) {
-
-        animal.coos
-        console.log('This is a pigeon')
-        return
+        animal.coos;
+        console.log('This is a pigeon');
+        return;
     }
-
     if ('meows' in animal) {
-
-        animal.meows
-        console.log('This is a cat')
-        return
+        animal.meows;
+        console.log('This is a cat');
+        return;
     }
-
 }
-
-
 // Part 2: Uncomment the lines below and test the classifyAnimal function by creating dog, pigeon, and cat objects
-const fido: dog = {
+var fido = {
     name: 'fido',
     color: 'gold',
     barks: true,
     hasTail: true
-}
-classifyAnimal(fido) // should print 'This is a dog'
-
+};
+classifyAnimal(fido); // should print 'This is a dog'
 // const whiskers = {
 // }
 // classifyAnimal(whiskers) // should print 'This is a cat'
-
 // const skyrat = {
-
 // }
 // classifyAnimal(skyrat) // should print 'This is a pigeon'
