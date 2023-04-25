31f7beb25871547237f598ceef4d9d529cfb93fb
sean <midruss@yahoo.com>
2023-04-25 18:57:07 -0400

    solve ts longest prefix problem

--- Practice-problems/src/longest_prefix.js
+++ Practice-problems/src/longest_prefix.js
@@ -1,46 +1,33 @@
 // Write a function to find the longest common prefix string amongst an array of strings.
 // If there is no common prefix, return an empty string "".
-
 // convert bellow javascript solution into typescript
 // var longestCommonPrefix = function(strs) {
-
 //     let prefix = strs[0]
 //     for(let i = 1; i < strs.length; i++){
-
 // 	   let index = 0;
 // 	   let word = ''
 // 	   while((prefix[index] && strs[i][index])&& prefix[index] === strs[i][index]){
-
 //         word += strs[i][index]
 //         index++
 // 	  }
 //          prefix = word;
 //     }
 //     return  prefix
-
 // }
-
 /*------------------------------------------write code here-----------------------------------------------*/
-
-const longestCommonPrefix = (strs: string[]):string => {
-    let prefix: string = strs[0] // "flow"
-
-    for (let i = 1; i < strs.length; i++) {
-        let index: number = 0 // 4
-        let word: string = '' // flow
+var longestCommonPrefix = function (strs) {
+    var prefix = strs[0]; // "flow"
+    for (var i = 1; i < strs.length; i++) {
+        var index = 0; // 4
+        var word = ''; // flow
         while ((prefix[index] && strs[i][index]) && prefix[index] === strs[i][index]) {
-            word += strs[i][index]
-            index++
+            word += strs[i][index];
+            index++;
         }
-        prefix = word
+        prefix = word;
     }
-    return prefix
-}
-
-
-
-
+    return prefix;
+};
 // -----------------------------------------Uncomment testcases-------------------------------------------------------
-
-console.log(longestCommonPrefix(["flower","flow","flight"])) //"fl"
-console.log(longestCommonPrefix(["dog","racecar","car"])) //""
+console.log(longestCommonPrefix(["flower", "flow", "flight"])); //"fl"
+console.log(longestCommonPrefix(["dog", "racecar", "car"])); //""
