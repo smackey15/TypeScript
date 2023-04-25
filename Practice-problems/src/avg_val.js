05651c47de448425fb1243743eed60e959ab9166
sean <midruss@yahoo.com>
2023-04-25 17:40:58 -0400

    complete ts avg val problem

--- Practice-problems/src/avg_val.js
+++ Practice-problems/src/avg_val.js
@@ -1,34 +1,25 @@
 // Write a function avgVal(arr) that accepts an array as an arg. The function should return the average of all values in the array.
 // If the array is empty, it should return null.
 // convert bellow javascript solution into typescript
-
 // function avgVal (arr){
 //     let sum= 0;
 //     if(!(arr.length)) return 0;
 //    for(let i=0; i<arr.length; i++){
 //      sum += arr[i];
-
 //    }
 //     return sum/arr.length
 //   }
-
 /*------------------------------------------write code here-----------------------------------------------*/
-
-function avgVal(arr: number[]):number {
-    let sum:number = 0;
-    if (!(arr.length)) return 0;
-    for (let i=0; i<arr.length; i++) {
-        sum += arr[i]
+function avgVal(arr) {
+    var sum = 0;
+    if (!(arr.length))
+        return 0;
+    for (var i = 0; i < arr.length; i++) {
+        sum += arr[i];
     }
-    return sum/arr.length
+    return sum / arr.length;
 }
-
-
-
-
-
 // -----------------------------------------Uncomment testcases-------------------------------------------------------
-
 console.log(avgVal([5, 10])); // 7.5
 console.log(avgVal([3, 7, 2, 1, 2])); // 3
 console.log(avgVal([])); // 0
