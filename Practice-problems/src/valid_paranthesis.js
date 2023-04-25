b563a0d55b710d8d1390490d5759180f37267e2d
sean <midruss@yahoo.com>
2023-04-25 18:19:25 -0400

    solve ts valid paranthesis problem

--- Practice-problems/src/valid_paranthesis.js
+++ Practice-problems/src/valid_paranthesis.js
@@ -1,14 +1,9 @@
 // Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-
 // An input string is valid if:
-
 // Open brackets must be closed by the same type of brackets.
 // Open brackets must be closed in the correct order.
 // Every close bracket has a corresponding open bracket of the same type.
-
-
 // convert bellow javascript solution into typescript
-
 // var isValid = function(s) {
 //     let pair ={"(":")", "{":"}", "[":"]"}
 //     let stack = []
@@ -24,35 +19,28 @@
 //     }
 //     return stack.length === 0
 // };
-
 /*------------------------------------------write code here-----------------------------------------------*/
-
-const isValid = (s:string):boolean => {
-    const pair: Record<string,string> = {
-        '(':')',
-        '[':']',
-        '{':'}'
-    }
-    const stack: string[] = []
-
-    for (let i=0; i<s.length; i++) {
-        if(s[i] in pair) {
-            stack.push(s[i])
-        } else {
-            const current = stack.pop()
-            if(current && pair[current] !== s[i]) {
-                return false
+var isValid = function (s) {
+    var pair = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    };
+    var stack = [];
+    for (var i = 0; i < s.length; i++) {
+        if (s[i] in pair) {
+            stack.push(s[i]);
+        }
+        else {
+            var current = stack.pop();
+            if (current && pair[current] !== s[i]) {
+                return false;
             }
         }
     }
-    return stack.length === 0
-}
-
-
-
-
+    return stack.length === 0;
+};
 // -----------------------------------------Uncomment testcases-------------------------------------------------------
-
-console.log(isValid("()")) //true
-console.log(isValid("()[]{}")) //true
-console.log(isValid("(]")) //false
+console.log(isValid("()")); //true
+console.log(isValid("()[]{}")); //true
+console.log(isValid("(]")); //false
